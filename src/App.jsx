d3bdc5006b7c40447b991174db45b6302118f396
Kanitin <kanitin.ju@gmail.com>
2026-01-22 19:00:45 +0700

    Revert "Feat/view post page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { Routes, Route } from "react-router-dom"
-import { Toaster } from "sonner"
-import { NavBarMain } from "./components/Navbar"
-import { Footer } from "./components/Footer"
-import { LandingPage } from "./pages/LandingPage"
-import { ViewPostPage } from "./pages/ViewPostPage"
-import { NotFoundPage } from "./pages/NotFoundPage"
+import StyleGuide from './components/StyleGuide'
 
 function App() {
   return (
-    <div className="flex flex-col bg-white min-h-screen">
-      <header className="w-full">
-        <NavBarMain />
-      </header>
-      <main className="grow">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/post/:postId" element={<ViewPostPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <div className="h-20" />
-      </main>
-      <Footer />
-      <Toaster />
-    </div>
+    <StyleGuide />
   )
 }
-
 
 export default App
--- src/lib/dateUtils.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(isoString) {
-    if (!isoString) return "";
-    const date = new Date(isoString);
-    return date.toLocaleDateString("en-GB", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    });
-}
